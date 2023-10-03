19d301b379a560818b0f1198efc0ba1fe1b7729a
jacobherman5409 <jacobherman5409@gmail.com>
2023-10-03 12:50:34 +0300

    ui creation

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>visuallearning</title>
+</head>
+<body>
+    <div>
+        <form>
+            <fieldset>
+                <label for=fname>username:</label><br>
+                <input type=text id=fname name=fname><br>
+                <label for=pwd>password:</label><br>
+                <input type=text id=pwd name=pwd><br>
+                <input type=submit value=submit>
+
+
+            </fieldset>
+        </form>
+
+    </div>
+</body>
+</html>
