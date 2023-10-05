ba051c8b5fe95ff7bbc14c622a69818b39e7b150
jacobherman5409 <jacobherman5409@gmail.com>
2023-10-05 12:53:38 +0300

    database connection

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "visual";
+
+//create connection
+$conn = mysqli_connect($servername,$username,$password,$db_name);
+
+// check connection
+
+if(!$conn){
+    die("connection failed:".mysqli_connect_errors());
+}
+echo"connection successfully";
+
+mysqli_close($conn);
+?>
